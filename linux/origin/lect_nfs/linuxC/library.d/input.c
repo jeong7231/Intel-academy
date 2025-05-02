edf38f7df20188e4dc4f03294508335c9afef50f
taeyun <jty7231@naver.com>
2025-05-02 12:23:17 +0900

    lecture

--- linux/origin/lect_nfs/linuxC/library.d/input.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int input()
-{
-	int num;
-	printf("Enter the num?");
-	scanf("%d",&num);
-	return num;
-}
--- linux/origin/lect_nfs/linuxC/make.d/output.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int output(int num)
-{
-	int i;
-	for(i=0; i<num; i++)
-		printf("Hi!\n");
-	return 0;
-}
